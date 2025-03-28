aaff78736a539920a124dd4082d8bd52ea2f49ff
anshul_1905 <anshul4510@users.noreply.github.com>
2025-03-28 17:52:59 +0530

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
 import {Routes,Route, useNavigate} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
@@ -20,11 +21,13 @@ const App = () => {
   return (
     <div>
       <ToastContainer theme='dark'/>
+      <HashRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/player/:id' element={<Player/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
+    </HashRouter>
     </div>
   )
 }
